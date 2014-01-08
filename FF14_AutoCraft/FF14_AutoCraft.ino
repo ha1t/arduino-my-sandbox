28c95ecb82b53c409ebac424ebc0d9d76d922347
ha1t <halt.feits@gmail.com>
2014-01-09 00:59:36 +0900

    不要なコメントの削除

--- FF14_AutoCraft/FF14_AutoCraft.ino
+++ FF14_AutoCraft/FF14_AutoCraft.ino
@@ -1,12 +1,4 @@
 
-
-// use this option for OSX. 
-// Comment it out if using Windows or Linux:
-char ctrlKey = KEY_LEFT_GUI;
-// use this option for Windows and Linux.
-// leave commented out if using OSX:
-//  char ctrlKey = KEY_LEFT_CTRL;    
-
 void setup() {
   // make pin 2 an input and turn on the 
   // pullup resistor so it goes high unless

66cbcd88624dfb881ee7dbaaf7526199f703d222
ha1t <halt.feits@gmail.com>
2015-08-11 15:11:05 +0900

    アノードコモンなので名前がおかしい。修正。

--- GM34031/GM34031.cpp
+++ GM34031/GM34031.cpp
@@ -3,11 +3,10 @@
 
 // Arduino IDE 1.6.4
 
-// PD0からPD7
-const int anode_pins[] = {0, 1, 2, 3, 4, 5, 6, 7};    // アノードに接続するArduinoのピン
-const int cathode_pins[] = {8, 9};  // カソードに接続するArduinoのピン
-const int number_of_anode_pins = sizeof(anode_pins) / sizeof(anode_pins[0]);
+const int cathode_pins[] = {0, 1, 2, 3, 4, 5, 6, 7};    // カソードに接続するArduinoのピン
+const int anode_pins[] = {8, 9};  // コモンに接続するArduinoのピン
 const int number_of_cathode_pins = sizeof(cathode_pins) / sizeof(cathode_pins[0]);
+const int number_of_anode_pins = sizeof(anode_pins) / sizeof(anode_pins[0]);
 int numbers_to_display = 0; // LEDに表示する数字を保持する変数
 
 // 下の桁から7セグのABC...
@@ -34,20 +33,20 @@ void display_numbers() {
   // number_to_displayの値を書き換えないために変数にコピー
   int n = numbers_to_display;
 
-  for (int i = 0; i < number_of_cathode_pins; i++) {
+  for (int i = 0; i < number_of_anode_pins; i++) {
 
     //PORTD = digits[n % 10];
-    for (int j = 0; j < number_of_anode_pins; j++) {
+    for (int j = 0; j < number_of_cathode_pins; j++) {
       if (bitRead(digits[n % 10], j)) {
-        digitalWrite(anode_pins[j], PIN_OFF);
+        digitalWrite(cathode_pins[j], PIN_OFF);
       } else {
-        digitalWrite(anode_pins[j], PIN_ON);
+        digitalWrite(cathode_pins[j], PIN_ON);
       }
     }
 
-    digitalWrite(cathode_pins[i], COMMON_ON);
+    digitalWrite(anode_pins[i], COMMON_ON);
     delayMicroseconds(300);
-    digitalWrite(cathode_pins[i], COMMON_OFF);
+    digitalWrite(anode_pins[i], COMMON_OFF);
     n = n / 10; // 10で割る
   }
 }
@@ -59,14 +58,14 @@ void set_numbers(int n) {
 // setup()　は，最初に一度だけ実行される
 void gm34031_setup() {
 
-  for (int i = 0; i < number_of_anode_pins; i++) {
-    pinMode(anode_pins[i], OUTPUT);
-    digitalWrite(anode_pins[i], PIN_OFF);
-  }
-
   for (int i = 0; i < number_of_cathode_pins; i++) {
     pinMode(cathode_pins[i], OUTPUT);
-    digitalWrite(cathode_pins[i], COMMON_OFF);
+    digitalWrite(cathode_pins[i], PIN_OFF);
+  }
+
+  for (int i = 0; i < number_of_anode_pins; i++) {
+    pinMode(anode_pins[i], OUTPUT);
+    digitalWrite(anode_pins[i], COMMON_OFF);
   }
 
 }

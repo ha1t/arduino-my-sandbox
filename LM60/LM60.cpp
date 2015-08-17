d76907cd5859648a4a284d10dba0aec54cf6a135
halt <halt.feits@gmail.com>
2015-08-17 21:51:00 +0900

    Update LM60.cpp
    vref説明追加

--- LM60/LM60.cpp
+++ LM60/LM60.cpp
@@ -4,15 +4,17 @@
 // LM60
 // @url http://brown.ap.teacup.com/nekosan0/681.html
 // @url http://ken-nou-kou.blogspot.com/2009/06/lm60biz.html
-
+// vrefは5150,2560,1100のいずれか
 float lm60_get_temp(int pin, int vref) {
 
   long mV = 0;
   int sum_v = 0;
+  int LOOP_COUNT = 5;
 
-  for (int i = 0; i < 11; i++) {
+  // 1回目は捨てるから一回多くまわる
+  for (int i = 0; i <= LOOP_COUNT; i++) {
 
-    // 一回目は捨てる
+    // 1回目は捨てる
     if (i == 0) {
         analogRead(pin);
         continue;
@@ -22,8 +24,6 @@ float lm60_get_temp(int pin, int vref) {
     delay(10);
   }
 
-  //mV = map(sum_v / 10, 0, 1023, 0, 5150); //←測定結果を電圧に変換（0-1023の変化を 0V-1.1mV へ）
-  mV = map(sum_v / 10, 0, 1023, 0, vref); //←測定結果を電圧に変換（0-1023の変化を 0V-1.1mV へ）
-  //mV = map(sum_v / 20, 0, 1023, 0, 1100); //←測定結果を電圧に変換（0-1023の変化を 0V-1.1mV へ）
+  mV = map(sum_v / LOOP_COUNT, 0, 1023, 0, vref); //←測定結果を電圧に変換（0-1023の変化を 0V-1.1mV へ）
   return (mV - 424) / 6.25;
 }

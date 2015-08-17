49a368556e07f97e1c7792efffa536df187a0b5d
halt <halt.feits@gmail.com>
2015-08-17 21:52:18 +0900

    set vref

--- LM60/examples/GetTemp/GetTemp.ino
+++ LM60/examples/GetTemp/GetTemp.ino
@@ -1,10 +1,12 @@
 #include <LM60.h>
 
 void setup() {
+    analogReference(INTERNAL);
     Serial.begin(9600);
 }
 
 void loop() {
-    Serial.println(lm60_get_temp(A1));
+    // Serial.println(lm60_get_temp(A1, 1100)); // for 328P
+    Serial.println(lm60_get_temp(A1, 2560)); // for 32u4
     delay(1000);
 }

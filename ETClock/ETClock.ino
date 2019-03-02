f2d06e6daebfec9f103ad91f74089f30f5e8fb06
halt <ha1t@users.noreply.github.com>
2019-03-03 01:27:06 +0900

    Update ETClock.ino
    use serial

--- ETClock/ETClock.ino
+++ ETClock/ETClock.ino
@@ -1,89 +1,95 @@
+#include <Wire.h>
+#include <Time.h>
+#include <DS1307RTC.h>
+#include <SevSeg.h>
+#include <FlexiTimer2.h>
 
-// DS1302_Serial_Easy (C)2010 Henning Karlsen
-// web: http://www.henningkarlsen.com/electronics
-//
-// A quick demo of how to use my DS1302-library to 
-// quickly send time and date information over a serial link
-//
-// I assume you know how to connect the DS1302.
-// DS1302:  CE pin    -> Arduino Digital 2
-//          I/O pin   -> Arduino Digital 3
-//          SCLK pin  -> Arduino Digital 4
+SevSeg sevseg; //Instantiate a seven segment object
 
-#include <DS1302.h>
-#include <BM428G.h>
+// #define USE_SERIAL
 
-// Init the DS1302
-DS1302 rtc(2, 3, 4);
+void setup() {
+
+#ifdef USE_SERIAL
+  Serial.begin(9600);
+  while (!Serial) ; // wait for serial
+  delay(200);
+  Serial.println("DS1307RTC Read Test");
+  Serial.println("-------------------");
+#endif
+
+  byte numDigits = 4;
+  byte digitPins[] = {10, 14, 15, 16};
+  byte segmentPins[] = {0, 1, 4, 5, 6, 7, 8, 9};
+  sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
+  sevseg.setNumber(10,0);
+  sevseg.setBrightness(20);
+  FlexiTimer2::set(10, flash);
+  FlexiTimer2::start();
+}
 
 // RealTime -> EorzeaTime
-int convert(int hours, int minutes, int seconds) {
-    int now_time = (hours * 3600) + (minutes * 60) + seconds;
+int eorzea_time_convert() {
+    
+    unsigned long earth_time = now(); // JPの9時間;
+    
+    uint64_t eorzea_time = ((uint64_t)earth_time * 205714285) / 600000000; // 最後に小数点以下を落としてるせいで精度が落ちる
 
-    float et_hours = (float)now_time / 175;
-    int et_minutes = (int)((et_hours - floor(et_hours)) * 60);
+    //int et_hour = (unsigned long)((float)earth_time / ((float)3600.0 / ((float)1440.0 / (float)70.0))) % 24;
+    int et_hour = (unsigned long)((((uint64_t)earth_time * 205714285) / 36000000000) % 24);
+    
+    // int et_minute = (unsigned long)((float)earth_time / ((float)60.0 / ((float)1440.0 / (float)70.0))) % 60;
+    int et_minute = (unsigned long)(eorzea_time % 60);
+    
+    return et_hour * 100 + et_minute;    
+}
 
-    while (et_hours > 24) {
-        et_hours = et_hours - 24;
+int real_time() {
+  return hour() * 100 + minute();
+}
+
+void loop() {
+  tmElements_t tm;
+  int eorzea_time;
+
+  if (RTC.read(tm)) {
+    setTime(makeTime(tm));
+    eorzea_time = eorzea_time_convert();
+
+#ifdef USE_SERIAL
+    Serial.print("Ok, Time = ");
+    Serial.print(tm.Hour);
+    Serial.write(':');
+    Serial.print(tm.Minute);
+    Serial.write(':');
+    Serial.print(tm.Second);
+    Serial.print(", Date = ");
+    Serial.print(tmYearToCalendar(tm.Year));
+    Serial.write('-');
+    Serial.print(tm.Month);
+    Serial.write('-');
+    Serial.print(tm.Day);
+    Serial.print(" / ");
+    Serial.print(eorzea_time);
+    Serial.println();
+#endif
+    
+    sevseg.setNumber(eorzea_time, 2);
+    //sevseg.setNumber(real_time(), 2);
+    delay(300);
+  } else {
+    if (RTC.chipPresent()) {
+      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+      Serial.println("example to initialize the time and begin running.");
+      Serial.println();
+    } else {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+      Serial.println();
     }
-
-    return (((int)et_hours * 100) + et_minutes);
+    delay(10000);
+  }
 }
 
-void setup()
-{
-  // Set the clock to run-mode, and disable the write protection
-  //rtc.halt(false);
-  //rtc.writeProtect(false);  
-
-  // The following lines can be commented out to use the values already stored in the DS1302
-  rtc.setDOW(FRIDAY);        // Set Day-of-Week to FRIDAY
-  rtc.setTime(17, 40, 0);     // Set the time to 12:00:00 (24hr format)
-  rtc.setDate(14, 8, 2015);   // Set the date to August 6th, 2010
-
-  //bm428g_setup();
-  pinMode(11, OUTPUT);
-  digitalWrite(11, HIGH);
-
-  // Setup Serial connection
-  Serial.begin(9600);
-/*
-  // f = クロック周波数 / ( 2 * 分周比　*　( 1 + 比較レジスタの値))
-  // 分周比=32, 比較レジスタの1値=255 -> f = 16000000 / (2 * 32 * 256) = 976 Hz
-  OCR1A = 255; // 255クロックごとに割り込みをかける
-  TCCR1B = 0b100; // 分周比を32に設定する
-  bitWrite(TIMSK1, OCIE1A, 1); // TIMER2を
-*/
+void flash() {
+  sevseg.refreshDisplay();
 }
-
-void loop()
-{
-  Time now = rtc.getTime();
-  
-  set_numbers(convert(now.hour, now.min, now.sec));
-  // Send Day-of-Week
-  //Serial.print(rtc.getDOWStr());
-
-  Serial.print(now.hour);
-  Serial.print(" -- ");
-
-  Serial.print(now.min);
-  Serial.print(" -- ");
-
-  Serial.print(now.sec);
-  Serial.print(" -- ");
-// Send date
-  //Serial.print(rtc.getDateStr());
-  //Serial.print(" -- ");
-
-  // Send time
-  //Serial.println(rtc.getTimeStr());
-  
-  // Wait one second before repeating :)
-  delay (1000);
-}
-
-ISR(TIMER1_COMPA_vect) {
-  display_numbers();
-}
-

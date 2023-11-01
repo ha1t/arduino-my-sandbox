810f1958112d6caa5fc3b5c4bd8845b9cbb5edd6
halt <ha1t@users.noreply.github.com>
2023-11-01 23:31:38 +0900

    Create mousemove.ino

---
+++ mousemove/mousemove.ino
@@ -0,0 +1,16 @@
+#include <Mouse.h>
+
+void setup() {
+  Mouse.begin();
+}
+
+void loop() {
+  Mouse.move(2, 2);
+  delay(25);
+  Mouse.move(0, 0);
+  delay(25);
+  Mouse.move(-2, -2);
+  delay(25);
+  Mouse.move(0,0);
+  delay(25);
+}
